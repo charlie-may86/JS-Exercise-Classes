1233bed111739650631d2cd0577ed401f7887021
Charlie May <camiv95@gmail.com>
2021-02-16 16:16:39 -0600

    more tests passed

--- index.js
+++ index.js
@@ -83,12 +83,20 @@ class Airplane {
       this.tank = 0;
       this.odometer = 0;
     }
+    fill(gallons) {
+      this.tank += gallons;
+    }
+
     drive(distance) {
-      this.odometer += distance;
-      this.tank -= (distance / this.milesPerGallon)
+      if(this.tank > 0){
+        this.odometer += distance;
+        this.tank -= (distance / this.milesPerGallon)
+      }
     }
 
   }
+
+
   
   /*
     TASK 3
@@ -103,7 +111,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attrs) {
+      this.name = attrs.name;
+      this.age = attrs.age;
+      this.location = attrs.location;
+    }
+    speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
   }
   
   /*
@@ -120,7 +135,19 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
+   constructor(attrs) {
+     super(attrs);
+     this.specialty = attrs.specialty;
+     this.favLanguage = attrs.favLanguage;
+     this.catchPhrase = attrs.catchPhrase;
+   }
+   demo(subject) {
+    return `Today we are learning about ${subject}`
+   }
+   grade(student, string) {
+    return `${student.name} receives a perfect score on ${string}`
+   }
 
  }
   /*
