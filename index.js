e995d483d2f52a6bdb35599be99ad2ba71749eb0
Charlie May <camiv95@gmail.com>
2021-02-16 15:38:48 -0600

    first class tests completed

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ class Airplane {
   */
   
  class Person {
-    
+   constructor(name, age) {
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+   }
+   eat(someFood) {
+     if(this.stomach.length < 10){
+       this.stomach.push(someFood);
+     }
+   }
+   
+   poop() {
+     return this.stomach = [];
+   }
+   toString() {
+     return `${this.name}, ${this.age}`
+   }
+   
   }
   
   /*
@@ -60,7 +77,17 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    drive(distance) {
+      this.odometer += distance;
+      this.tank -= (distance / this.milesPerGallon)
+    }
+
   }
   
   /*
